8eb3759c098fdc03654c769c549987e2839f76a3
iamezell <iamezell@gmail.com>
2019-05-05 19:15:50 -0400

    adding stuff

---
+++ public/precache-manifest.e09c813d55e4f12fc4b37471b3e09c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c6768b22a7ee849919822d3791c929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23ea4e83fe1d7f944a99",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "23ea4e83fe1d7f944a99",
+    "url": "/static/js/main.eb7a9e6d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
